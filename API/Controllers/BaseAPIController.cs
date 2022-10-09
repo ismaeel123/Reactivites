3f56b033885488624e940dfb60cbec4080fcb8c3
ismaeel123 <37120315+ismaeel123@users.noreply.github.com>
2022-10-10 00:52:09 +0200

    End Of Section 2

---
+++ API/Controllers/BaseAPIController.cs
@@ -0,0 +1,14 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseAPIController : ControllerBase
+    {
+        
+    }
+}
